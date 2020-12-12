c8a367931b7d2b0cac97c69808198431461f624a
angelynaliem <angelynaliem@gmail.com>
2020-12-11 22:27:36 -0500

    Added config folder

--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,48 @@
 // Update with your config settings.
 
-const pgConnection = (process.env.DATABASE_URL || module.exports = {
+const pgConnection = process.env.DATABASE_URL; //add || postgres path later
+
+module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3",
+      filename: "./data/bwbe.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 
-  staging: {
-    client: "postgresql",
+  testing: {
+    client: "sqlite3",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      filename: "./data/testbwbe.db3",
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    client: "pg",
+    connection: pgConnection,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
-});
+};
