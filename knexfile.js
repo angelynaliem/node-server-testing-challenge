840009a53a29c61640b20fd6370f0c41a6142caf
angelynaliem <angelynaliem@gmail.com>
2020-12-12 10:25:28 -0500

    Updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 // Update with your config settings.
 
-const pgConnection = process.env.DATABASE_URL; //add || postgres path later
+const pgConnection =
+  process.env.DATABASE_URL ||
+  "postgres://swngzftqsfahka:cba75265a676d18a13e2b069b6fc210e30548bd7fb48ea53c85c2d57442b3c2e@ec2-54-157-66-140.compute-1.amazonaws.com:5432/d6gjkdkn27i8ni";
 
 module.exports = {
   development: {
