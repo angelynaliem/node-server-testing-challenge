7eaa3f6777918cf9674b7c79732c1282d5c29d2a
angelynaliem <angelynaliem@gmail.com>
2020-12-12 11:06:13 -0500

    Updated users router

--- users/users-router.js
+++ users/users-router.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-const restricted = require("../auth/restricted-middleware.js");
+// const restricted = require("../auth/restricted-middleware.js");
 
-router.get("/", restricted, async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await Users.find();
     res.status(200).json(users);
