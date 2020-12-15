b9cce87f186d101e9a2c05f058fe1e850ef6f042
angelynaliem <angelynaliem@gmail.com>
2020-12-15 15:25:48 -0500

    Updated connection.js

--- data/connection.js
+++ data/connection.js
@@ -2,6 +2,8 @@ const knex = require("knex");
 
 const config = require("../knexfile.js");
 
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.NODE_ENV || "development";
+
+console.log(process.env.NODE_ENV);
 
 module.exports = knex(config[environment]);
